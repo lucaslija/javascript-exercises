0fcdb1b6958680833fd68058a37090bc584c596b
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-07-31 18:58:28 -0700

    Attempted to solve repeatString w/for loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    responseString = '';
+    for (let i = 0; i < 3; i++) {
+        responseString += string;
+    }
+    return responseString;
 };
 
 // Do not edit below this line
