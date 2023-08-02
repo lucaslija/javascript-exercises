10a19fbf5c8b9cb12d7a730ade9e7cbc5d629820
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-08-01 18:43:05 -0700

    03 reverses single word

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const chars = string.split('');
+    let reversed = [];
+    for (let i = chars.length; i >= 0; i--) {
+        reversed.push(chars[i]);
+    }
+    const newString = reversed.join('');
+    return newString;
 };
 
 // Do not edit below this line
