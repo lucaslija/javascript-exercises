07523ff3106a661582b6eca5b19e6629919007ce
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-08-02 17:13:30 -0700

    04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    let ret = [];
+    for (each of array) {
+        if (!(elements.includes(each))) {
+            ret.push(each);
+        }
+    }
+    return ret;
 };
 
 // Do not edit below this line
