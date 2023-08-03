4236ea8acc673ce8de47b8f2c4865d60bb74f4be
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-08-02 17:40:10 -0700

    05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!(Number.isInteger(a) && Number.isInteger(b) && a >= 0 && b >= 0)) {
+        return "ERROR";
+    }
+    const lo = Math.min(a, b);
+    const hi = Math.max(a, b);
 
+    let sumUp = function(n) {
+        return ((n * (n + 1)) / 2)
+    }
+
+    return sumUp(hi) - sumUp(lo - 1);
 };
 
 // Do not edit below this line
