485b067d852d9220cc0b8667178ace078314a3d4
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-10-02 22:57:39 -0700

    12_findTheOldest completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const d = new Date();
+    let year = d.getFullYear();
 
+    let stillAlive = people.map(person => ({
+        name: person.name,
+        yearOfBirth: person.yearOfBirth,
+        yearOfDeath: (person.yearOfDeath ? person.yearOfDeath : year),
+    }));
+
+    const ordered = stillAlive.sort((b, a) =>
+    (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? 1 : -1);
+    return ordered[0];
 };
 
 // Do not edit below this line
